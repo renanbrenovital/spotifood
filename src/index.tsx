31170836106e0abbb477625a524c143eefee5c1b
Renan Vital <renanbreno@gmail.com>
2020-08-11 17:05:00 -0300

    install styled components and create global style

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`  
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }  
+  body {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    font-family: 'Poppins', sans-serif;
+    font-size: 62.5%;
+    font-style: normal;
+    font-weight: 400;
+    letter-spacing: 0;
+    transition: all 0.25s linear;
+  }
+`;
+
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
